bfac0427109e73909e7122cf58a76a74e80b46f7
nityapriya-sahu <sahunityapriya@gmail.com>
2024-09-25 23:06:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// var name = "Nitya Priya";
+
+var a = 50;
+var b = 20;
+// console.log(name);
+// console.log(a + b);
+
+// console.log(global); //gives global object
+
+// console.log(globalThis); // --do--
+
+// console.log(this); // gives empty object
+
+console.log(globalThis === global); //true
+
+console.log(global === this); //false
