cb610426d84efb091eba6c2cd4228e7385beedcb
nityapriya-sahu <sahunityapriya@gmail.com>
2024-09-27 23:52:34 +0530

    iife, new parameter module & require

--- Calculate/multiply.js
+++ Calculate/multiply.js
@@ -4,3 +4,18 @@ function calculateMultiply(a, b) {
 }
 
 module.exports = { calculateMultiply };
+
+//module: Node.js is adding module
+
+//===============================================
+
+// function wrapped inside a function, this function has a parameter named module. It's an object provided by Node.js include module.exports
+
+(function (module) {
+  //All code of module runs inside here
+  function calculateMultiply(a, b) {
+    let multiply = a * b;
+    console.log(multiply);
+  }
+  module.exports = { calculateMultiply };
+})(module);
